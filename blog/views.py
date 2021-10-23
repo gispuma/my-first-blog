5302db369155600121a458a041c4c75e8ae707b5
gisel <tendergis2@gmail.com>
2021-10-23 12:18:20 -0400

    agregando plantillas dinamicas

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
 
 # Create your views here.
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.all()
+    return render(request, 'blog/post_list.html', {'posts':posts})
 
 def pagina(request):
     return render(request, 'blog/pagina.html', {})
+
+def direccion (request):
+    return render(request, 'blog/direccion.html', {})
